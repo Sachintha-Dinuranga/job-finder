bfcb6f0e0ed19b0f758ef3182c194c64f7662843
Sachintha Dinuranga <117833055+Sachintha-Dinuranga@users.noreply.github.com>
2025-01-18 12:22:57 +0530

    add page

--- src/components/JobListings.jsx
+++ src/components/JobListings.jsx
@@ -1,19 +1,43 @@
 import React from "react";
-import jobs from "../jobs.json";
 import JobListing from "./JobListing";
-const JobListings = () => {
-  const recentJobs = jobs.slice(0, 3); // ui eke 3 i pennanne
+import { useState, useEffect } from "react";
+import Spineer from "./Spineer";
+const JobListings = ({ isHome = false }) => {
+  const [jobs, setJobs] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchJobs = async () => {
+      try {
+        const apiURL = isHome ? "/api/jobs?_limit=3" : "/api/jobs";
+        const res = await fetch(apiURL);
+        const data = await res.json();
+        setJobs(data);
+      } catch (error) {
+        console.log("error fetching data", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchJobs();
+  }, []);
+
   return (
     <section className="bg-blue-50 px-4 py-10">
       <div className="container-xl lg:container m-auto">
         <h2 className="text-3xl font-bold text-indigo-500 mb-6 text-center">
-          Browse Jobs
+          {isHome ? "Recent Jobs" : "Browse Jobs"}
         </h2>
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-          {recentJobs.map((job) => (
-            <JobListing key={job.id} job={job} /> //key has to be unique value, we need to add a key otherwise it create an error
-          ))}
-        </div>
+
+        {loading ? (
+          <Spineer loading={loading} />
+        ) : (
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+            {jobs.map((job) => (
+              <JobListing key={job.id} job={job} /> //key has to be unique value, we need to add a key otherwise it create an error
+            ))}
+          </div>
+        )}
       </div>
     </section>
   );
